b3d5e15fec2e493e0f354e012bd22bc8cace8d86
Søren F. Wünsche <sorenfw13@hotmail.com>
2018-05-15 22:27:48 +0200

    Igang med at konveretere til tabbed activity

---
+++ app/src/main/java/com/example/dndgenerator/Summary.java
@@ -0,0 +1,36 @@
+package com.example.dndgenerator;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class Summary extends AppCompatActivity {
+
+    TextView txtName, txtRace, txtClass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        txtName = findViewById(R.id.txtName);
+        txtRace = findViewById(R.id.txtRace);
+        txtClass = findViewById(R.id.txtClass);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
